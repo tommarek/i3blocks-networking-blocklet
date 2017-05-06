73fef6a88c68481e6d291430efe784528c604f1f
Tom Marek <tomas.marek@resta.cz>
2017-05-06 02:02:51 +0200

    fix indentation and signal colours

--- networking.py
+++ networking.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2017 Tom Marek <tm.tomas.marek@gmail.com>
 #
 # Thanks to: 
-#	http://code.activestate.com/recipes/439093-get-names-of-all-up-network-interfaces-linux-only/
+#    http://code.activestate.com/recipes/439093-get-names-of-all-up-network-interfaces-linux-only/
 #
 #------------------------------------------------------------------------
 # TODO: maybe add total up/download
@@ -18,9 +18,9 @@ import sys
 import time
 
 colours = {
-	'green': '#00FF00',
-	'orange': '#FF8000',
-	'red': '#FF0000',
+    'green': '#00FF00',
+    'orange': '#FF8000',
+    'red': '#FF0000',
 }
 
 
@@ -58,141 +58,141 @@ def get_default_route():
         for line in f:
             fields = line.strip().split()
             if fields[1] == '00000000' and int(fields[3], 16) & 2:
-	            return fields[0], socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
+                return fields[0], socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
 
 def get_iface_type(iface_name):
-	with open('/sys/class/net/%s/type' % iface_name, 'r') as f:
-		return f.readline().rstrip()
+    with open('/sys/class/net/%s/type' % iface_name, 'r') as f:
+        return f.readline().rstrip()
 
 def get_iface_operstate(iface_name):
-	with open('/sys/class/net/%s/operstate' % iface_name, 'r') as f:
-		operstate = f.readline().rstrip()
+    with open('/sys/class/net/%s/operstate' % iface_name, 'r') as f:
+        operstate = f.readline().rstrip()
 
-		if operstate == 'up':
-			colour = colours['green']
-		elif operstate == 'down':
-			colour = colours['red']
-		else:
-			colour = colours['orange']
+        if operstate == 'up':
+            colour = colours['green']
+        elif operstate == 'down':
+            colour = colours['red']
+        else:
+            colour = colours['orange']
 
-		return operstate, colour
+        return operstate, colour
 
 def get_iface_bandwith(iface_name):
-	iface_file_path = '/dev/shm/networking-%s' % iface_name
+    iface_file_path = '/dev/shm/networking-%s' % iface_name
 
-	# get current time
-	current_time = int(round(time.time()))
-	# get data from interface
-	with open('/sys/class/net/%s/statistics/rx_bytes' % iface_name, 'r') as f:
-		rx = int(f.readline().rstrip())
-	with open('/sys/class/net/%s/statistics/tx_bytes' % iface_name, 'r') as f:
-		tx = int(f.readline().rstrip())
+    # get current time
+    current_time = int(round(time.time()))
+    # get data from interface
+    with open('/sys/class/net/%s/statistics/rx_bytes' % iface_name, 'r') as f:
+        rx = int(f.readline().rstrip())
+    with open('/sys/class/net/%s/statistics/tx_bytes' % iface_name, 'r') as f:
+        tx = int(f.readline().rstrip())
 
-	if not os.path.isfile(iface_file_path):
-		with open(iface_file_path, 'w') as f:
-			f.write('%d,%d,%d' % (current_time, rx, tx))
-			old_time, old_rx, old_tx = current_time, rx, tx
+    if not os.path.isfile(iface_file_path):
+        with open(iface_file_path, 'w') as f:
+            f.write('%d,%d,%d' % (current_time, rx, tx))
+            old_time, old_rx, old_tx = current_time, rx, tx
 
-	with open(iface_file_path, 'r') as f:
-		old_time, old_rx, old_tx = f.readline().rstrip().split(',')
-	with open(iface_file_path, 'w') as f:
-		f.write('%d,%d,%d' % (current_time, rx, tx))
+    with open(iface_file_path, 'r') as f:
+        old_time, old_rx, old_tx = f.readline().rstrip().split(',')
+    with open(iface_file_path, 'w') as f:
+        f.write('%d,%d,%d' % (current_time, rx, tx))
 
-	time_delta = current_time - int(old_time) or 1
+    time_delta = current_time - int(old_time) or 1
 
-	rx_delta = rx - int(old_rx)
-	tx_delta = tx - int(old_tx)
+    rx_delta = rx - int(old_rx)
+    tx_delta = tx - int(old_tx)
 
-	rx_Bps = rx_delta / float(time_delta)
-	tx_Bps = tx_delta / float(time_delta)
+    rx_Bps = rx_delta / float(time_delta)
+    tx_Bps = tx_delta / float(time_delta)
 
-	return (rx_Bps, tx_Bps)
+    return (rx_Bps, tx_Bps)
 
 def get_wireless_info(iface):
-	if not os.path.isdir('/sys/class/net/%s/wireless' % iface):
-		return None
+    if not os.path.isdir('/sys/class/net/%s/wireless' % iface):
+        return None
 
-	with open('/proc/net/wireless', 'r') as f:
-		for line in f:
-			fields = re.sub(r'([^\s\w]|_)+', '', line).split()
-			if iface in fields:
-				signal = (float(fields[2]) * 100) / 70
-				if signal > 80:
-					colour = colours['green']
-				elif signal > 60:
-					colour = colours['orange']
-				if signal > 80:
-					colour = colours['red']
+    with open('/proc/net/wireless', 'r') as f:
+        for line in f:
+            fields = re.sub(r'([^\s\w]|_)+', '', line).split()
+            if iface in fields:
+                signal = (float(fields[2]) * 100) / 70
+                if signal > 75:
+                    colour = colours['green']
+                elif signal > 40:
+                    colour = colours['orange']
+                else:
+                    colour = colours['red']
 
-	ssid = subprocess.check_output(["iwgetid", "-r"], universal_newlines=True).rstrip()
+    ssid = subprocess.check_output(["iwgetid", "-r"], universal_newlines=True).rstrip()
 
-	return ssid, signal, colour
+    return ssid, signal, colour
 
 
 def _get_iface_string(iface):
-	if iface['wireless']:
-		return ' <span foreground="%s"><b>%s</b></span>(<span foreground="%s"><b>%s</b></span>, <span foreground="%s"><b>%d%%</b></span>): %s (%skB/%skB)' % (
-			iface['operstate'][1],
-			iface['iface'],
-			iface['operstate'][1],
-			iface['wireless'][0],
-			iface['wireless'][2],
-			iface['wireless'][1],
-			iface['ip'],
-			int(iface['bandwith'][0]/1000),
-			int(iface['bandwith'][1]/1000),
-		)
-	else:
-		return ' <span foreground="%s"><b>%s</b></span>: %s (%skB/%skB)' % (
-			iface['operstate'][1],
-			iface['iface'],
-			iface['ip'],
-			int(iface['bandwith'][0]/1000),
-			int(iface['bandwith'][1]/1000),
-		)
+    if iface['wireless']:
+        return ' <span foreground="%s"><b>%s</b></span>(<span foreground="%s"><b>%s</b></span>, <span foreground="%s"><b>%d%%</b></span>): %s (%skB/%skB)' % (
+            iface['operstate'][1],
+            iface['iface'],
+            iface['operstate'][1],
+            iface['wireless'][0],
+            iface['wireless'][2],
+            iface['wireless'][1],
+            iface['ip'],
+            int(iface['bandwith'][0]/1000),
+            int(iface['bandwith'][1]/1000),
+        )
+    else:
+        return ' <span foreground="%s"><b>%s</b></span>: %s (%skB/%skB)' % (
+            iface['operstate'][1],
+            iface['iface'],
+            iface['ip'],
+            int(iface['bandwith'][0]/1000),
+            int(iface['bandwith'][1]/1000),
+        )
 
 def print_out(to_print):
-	# print the physical interfaces
-	final_string = '<span font="FontAwesome">\uf0ac</span>'
-	if to_print['phy']:
-		for iface in sorted(to_print['phy']):
-			final_string += _get_iface_string(to_print['ifaces'][iface])
-	else:
-		final_string += ' None'
+    # print the physical interfaces
+    final_string = '<span font="FontAwesome">\uf0ac</span>'
+    if to_print['phy']:
+        for iface in sorted(to_print['phy']):
+            final_string += _get_iface_string(to_print['ifaces'][iface])
+    else:
+        final_string += ' None'
 
-	# then print all the logical interfaces (VPNs) excluding loopback
-	final_string += ' <span font="FontAwesome">\uf023</span>'
-	if len(to_print['log']) > 1:
-		for iface in sorted(to_print['log']):
-			# http://lxr.linux.no/#linux+v3.0/include/linux/if_arp.h
-			if to_print['ifaces'][iface]['iface_type'] != 772:
-				final_string += _get_iface_string(to_print['ifaces'][iface])
-	else:
-		final_string += ' None'
+    # then print all the logical interfaces (VPNs) excluding loopback
+    final_string += ' <span font="FontAwesome">\uf023</span>'
+    if len(to_print['log']) > 1:
+        for iface in sorted(to_print['log']):
+            # http://lxr.linux.no/#linux+v3.0/include/linux/if_arp.h
+            if to_print['ifaces'][iface]['iface_type'] != 772:
+                final_string += _get_iface_string(to_print['ifaces'][iface])
+    else:
+        final_string += ' None'
 
-	return final_string
+    return final_string
 
 def main():
-	to_print = {'phy': [], 'log': [], 'def':None, 'ifaces':{}}
-	default_route = get_default_route()
+    to_print = {'phy': [], 'log': [], 'def':None, 'ifaces':{}}
+    default_route = get_default_route()
 
-	ifaces = all_interfaces()
-	for iface, ip in ifaces:
-		if default_route and iface == default_route[0]:
-			to_print['def'] = iface
+    ifaces = all_interfaces()
+    for iface, ip in ifaces:
+        if default_route and iface == default_route[0]:
+            to_print['def'] = iface
 
-		iface_type = int(get_iface_type(iface))
-		to_print['phy' if iface_type == 1 else 'log'].append(iface)
-		to_print['ifaces'][iface] = {
-			'iface': iface,
-			'ip': ip,
-			'operstate': get_iface_operstate(iface),
-			'bandwith': get_iface_bandwith(iface),
-			'iface_type': iface_type,
-			'wireless': get_wireless_info(iface),
-		}
+        iface_type = int(get_iface_type(iface))
+        to_print['phy' if iface_type == 1 else 'log'].append(iface)
+        to_print['ifaces'][iface] = {
+            'iface': iface,
+            'ip': ip,
+            'operstate': get_iface_operstate(iface),
+            'bandwith': get_iface_bandwith(iface),
+            'iface_type': iface_type,
+            'wireless': get_wireless_info(iface),
+        }
 
-	print(print_out(to_print))
+    print(print_out(to_print))
 
 
 main()
